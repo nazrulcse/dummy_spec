c455c092fbe8997237e31a26f9a80391233b4d9b
nazrul <nazrulku07@gmail.com>
2015-08-15 12:44:31 +0600

    write test code for blog controller

--- spec/features/controller/blog_spec.rb
+++ spec/features/controller/blog_spec.rb
@@ -11,4 +11,61 @@ describe 'Test User Blog', :type => :controller do
     expect(page).to have_content @blog.title.to_s
   end
 
+  it 'It should visit blog new page' do
+    visit '/blogs/new'
+    expect(page).to have_content 'New Blog'
+  end
+
+  it 'It should validate blog title' do
+    visit '/blogs/new'
+    within('#new_blog') do
+      fill_in 'Description', :with => 'Sample Description'
+      fill_in 'User', :with => @user.id
+    end
+    click_button 'Create Blog'
+    expect(page).to have_content "Title can't be blank"
+  end
+
+  it 'It should validate blog description' do
+    visit '/blogs/new'
+    within('#new_blog') do
+      fill_in 'Title', :with => 'Test Blog'
+      fill_in 'User', :with => @user.id
+    end
+    click_button 'Create Blog'
+    expect(page).to have_content "Description can't be blank"
+  end
+
+  it 'It should create a new blog and redirect to show page' do
+    visit '/blogs/new'
+    within('#new_blog') do
+      fill_in 'Title', :with => 'Test Blog'
+      fill_in 'Description', :with => 'Sample Description'
+      fill_in 'User', :with => @user.id
+    end
+    click_button 'Create Blog'
+    expect(page).to have_content 'Blog was successfully created'
+  end
+
+  it 'It should redirect to edit page' do
+    visit "/blogs/#{@blog.id}/edit"
+    expect(page).to have_content 'Editing Blog'
+  end
+
+  it 'It should update blog and redirect to show page' do
+    visit "/blogs/#{@blog.id}/edit"
+    new_title = 'This is new title'
+    within('.edit_blog') do
+      fill_in 'Title', :with => new_title
+    end
+    click_button 'Update Blog'
+    expect(page).to have_content new_title.to_s
+  end
+
+  it 'It should delete blog and redirect to index page' do
+    visit '/blogs'
+    click_link "delete-blog-#{@blog.id}"
+    expect(page).to have_content 'Blog was successfully destroyed'
+  end
+
 end
